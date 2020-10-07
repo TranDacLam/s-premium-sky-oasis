9c9f31d08143f882e1425cbd5997f04f4ce09382
TranDacLam <tdlam2105@gmail.com>
2020-10-08 01:36:52 +0700

    #7

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ $(function(){
 		}
 	}
 
-	addFullPage()
+	// addFullPage()
 	
 
 	/* Start slide */
@@ -68,7 +68,7 @@ $(function(){
 		stagePadding: 200,
 		nav: true,
 		navText: navText,
-		// autoplay:true,
+		autoplay:true,
 		autoplayTimeout:5000,
 		autoplayHoverPause:true,
 		responsive:{
@@ -89,38 +89,38 @@ $(function(){
 		}
 	});
 
-	$('#section0').parallax("50%", 0.1);
-	$('#section1').parallax("50%", 0.1);
-	$('#section2').parallax("50%", 0.1);
-	$('#section3').parallax("50%", 0.1);
-	$('#section4').parallax("50%", 0.1);
-	$('#section5').parallax("50%", 0.1);
-	$('#section6').parallax("50%", 0.1);
-	$('#section7').parallax("50%", 0.1);
-	$('#section8').parallax("50%", 0.1);
+	// $('#section0').parallax("50%", 0.1);
+	// $('#section1').parallax("50%", 0.1);
+	// $('#section2').parallax("50%", 0.1);
+	// $('#section3').parallax("50%", 0.1);
+	// $('#section4').parallax("50%", 0.1);
+	// $('#section5').parallax("50%", 0.1);
+	// $('#section6').parallax("50%", 0.1);
+	// $('#section7').parallax("50%", 0.1);
+	// $('#section8').parallax("50%", 0.1);
 
 	/* Start Scroll fixed Menu */
-	var header = document.getElementById("landing-nav");
-	var sticky = header.offsetTop;
-	window.onscroll = function() {
-		myFunction()
-		let btnTop = $('.scroll-to-top');
-		if (window.scrollY >= 300) {
-			btnTop.addClass('is-visible');
-		} else {
-			btnTop.removeClass('is-visible');
-		} 
-	};
+	// var header = document.getElementById("landing-nav");
+	// var sticky = header.offsetTop;
+	// window.onscroll = function() {
+	// 	myFunction()
+	// 	let btnTop = $('.scroll-to-top');
+	// 	if (window.scrollY >= 300) {
+	// 		btnTop.addClass('is-visible');
+	// 	} else {
+	// 		btnTop.removeClass('is-visible');
+	// 	} 
+	// };
 
-	myFunction()
+	// myFunction()
 
-	function myFunction() {
-		if (window.pageYOffset > sticky) {
-			header.classList.add("sticky-top");
-		} else {
-			header.classList.remove("sticky-top");
-		}
-	}
+	// function myFunction() {
+	// 	if (window.pageYOffset > sticky) {
+	// 		header.classList.add("sticky-top");
+	// 	} else {
+	// 		header.classList.remove("sticky-top");
+	// 	}
+	// }
 	/* End Scroll fixed Menu */
 
 	$('.scroll-to-top').on('click', function(){
@@ -133,17 +133,72 @@ $(function(){
 	$("#navbar-menu a").on('click', function(event) {
 		var _this = $(this);
 		$('html, body').stop().animate({ scrollTop: $(_this.attr('href')).offset().top }, 1500);
-		if(!$(this).hasClass('collapsed') && $(window).width() <= 992){
-			$('.navbar-toggler').trigger('click');
-		}
+		$('.navbar-toggler').trigger('click');
 		event.preventDefault();
 	});
+	  
+	//   OpacityScroll.init('s-premium');
+	function scrollFullPage() {
+		var isScroll = false
+		var allEle = $(`.s-premium .section`)
+		var index = 0
+		var maxIndex = allEle.length
+		function opacityScroll(){
+			[].forEach.call(allEle, (child, idx) => {
+				console.log(idx, child)
+				if(idx == index){
+					$(child).css({'z-index': 2})
+				}else{
+					$(child).css({'transform': 'translate(0%, 100%)'})
+				}
+			})
+		}
+		opacityScroll()
 
-	// function checkScreen(){
-		
-	// }
+		$(window).on('wheel', function (e) { 
+			// your logic here
+			var delta = e.originalEvent.wheelDelta
+			if(isScroll || (index <= 0 && delta > 0) || (index >= (maxIndex - 1) && delta < 0)){
+				return
+			}
+			isScroll = true
+			if (delta < 0) {
+				console.log("Scroll down");
+				let indexTemp = index
+				index += 1
+				$(allEle[index]).css({'z-index': 2, 'transform': 'translate(0%, 0%)'})
+				$(allEle[indexTemp]).css({'z-index': 1})
+				setTimeout(() => {
+					$(allEle[indexTemp]).css({'transform': 'translate(0%, -100%)'})
+				}, 800)
+			} else {
+				console.log("Scroll up");
+				let indexTemp = index
+				index -= 1
+				$(allEle[index]).css({'z-index': 2, 'transform': 'translate(0%, 0%)'})
+				$(allEle[indexTemp]).css({'z-index': 1})
+				setTimeout(() => {
+					$(allEle[indexTemp]).css({'transform': 'translate(0%, 100%)'})
+				}, 800)
+			}
+			setTimeout(() => {
+				isScroll = false
+			}, 1600)
+		})
 
-	// $(window).on('resize', function(){
-	// 	console.log(111)
-	// })
+		$('#navbar-box-menu').on('click', 'a', function(e){
+			let indexTagA = parseInt($(this).attr('data-index'));
+			let indexTemp = index
+			index = indexTagA
+			$(allEle[index]).css({'z-index': 2, 'transform': 'translate(0%, 0%)'})
+			$(allEle[indexTemp]).css({'z-index': 1})
+			setTimeout(() => {
+				$(allEle[indexTemp]).css({'transform': 'translate(0%, -100%)'})
+			}, 800)
+			console.log(index)
+		})
+	}
+
+	scrollFullPage()
+	  
 })
